adb83f0f682cbcb1abc0fa051f549463e64f6885
shorres <stratton.horres@prizepicks.com>
2025-05-20 21:49:14 -0500

    Net new data; unit type and faction data

---
+++ src/data/BlackGrailUnits.js
@@ -0,0 +1,38 @@
+export const LORD_OF_TUMOURS = 'Lord of Tumours';
+export const PLAGUE_KNIGHT = 'Plague Knight';
+export const CORPSE_GUARD = 'Corpse Guard';
+export const HOUND_OF_THE_BLACK_GRAIL = 'Hound of the Black Grail';
+export const GRAIL_THRALL = 'Grail Thrall';
+export const FLY_THRALL = 'Fly Thrall';
+export const HERALD_OF_BEELZEBUB = 'Herald of Beelzebub';
+export const AMALGAM = 'Amalgam';
+
+export const soldierTypes = [LORD_OF_TUMOURS,PLAGUE_KNIGHT,CORPSE_GUARD,
+    HOUND_OF_THE_BLACK_GRAIL,GRAIL_THRALL,FLY_THRALL,HERALD_OF_BEELZEBUB,AMALGAM,];
+
+  export default {
+  [LORD_OF_TUMOURS]: {
+    name: 'Lord of Tumours',
+  },
+  [PLAGUE_KNIGHT]: {
+    name: 'Plague Knight',
+  },
+  [CORPSE_GUARD]: {
+    name: 'Corpse Guard',
+  },
+  [HOUND_OF_THE_BLACK_GRAIL]: {
+    name: 'Hound of the Black Grail',
+  },
+  [GRAIL_THRALL]: {
+    name: 'Grail Thrall',
+  },
+  [FLY_THRALL]: {
+    name: 'Fly Thrall',
+  },
+  [HERALD_OF_BEELZEBUB]: {
+    name: 'Herald of Beelzebub',
+  },
+  [AMALGAM]: {
+    name: 'Amalgam',
+  }
+};
---
+++ src/data/TrenchPilgrimUnits.js
@@ -0,0 +1,35 @@
+export const WAR_PROPHET = 'War Prophet';
+export const CASTIGATOR = 'Castigator';
+export const COMMUNICANT = 'Communicant';
+export const TRENCH_PILGRIM = 'Trench Pilgrim';
+export const ECCLESIASTIC_PRISONER = 'Ecclesiastic Prisoner';
+export const STIGMATIC_NUN = 'Stigmatic Nun';
+export const SHRINE_ANCHORITE = 'Shrine Anchorite';
+
+export const soldierTypes = [WAR_PROPHET,CASTIGATOR,COMMUNICANT,TRENCH_PILGRIM,
+    ECCLESIASTIC_PRISONER,STIGMATIC_NUN,SHRINE_ANCHORITE,
+];
+
+  export default {
+  [WAR_PROPHET]: {
+    name: 'War Prophet',
+  },
+  [CASTIGATOR]: {
+    name: 'Castigator',
+  },
+  [COMMUNICANT]: {
+    name: 'Communicant',
+  },
+  [TRENCH_PILGRIM]: {
+    name: 'Trench Pilgrim',
+  },
+  [ECCLESIASTIC_PRISONER]: {
+    name: 'Ecclesiastic Prisoner',
+  },
+  [STIGMATIC_NUN]: {
+    name: 'Stigmatic Nun',
+  },
+  [SHRINE_ANCHORITE]: {
+    name: 'Shrine Anchorite',
+  }
+};
