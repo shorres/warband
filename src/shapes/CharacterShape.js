0fabefcc76abea4d4fb641ab67770f8fec622028
shorres <stratton.horres@prizepicks.com>
2025-05-20 21:49:50 -0500

    Adjusted props

--- src/shapes/CharacterShape.js
+++ src/shapes/CharacterShape.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { ABILITIES, ARMOUR, BASE, KEYWORDS, MELEE, MOVEMENT, RANGED } from '../data/Misc';
+import { ARMOUR, BASE, KEYWORDS, MELEE, MOVEMENT, RANGED } from '../data/Misc';
 
 export default {
   name: PropTypes.string,
@@ -9,6 +9,4 @@ export default {
   [ARMOUR]: PropTypes.number.isRequired,
   [BASE]: PropTypes.number.isRequired,
   [KEYWORDS]: PropTypes.string.isRequired,
-  [ABILITIES]: PropTypes.array,
-  EQUIPMENT: PropTypes.array
 };
