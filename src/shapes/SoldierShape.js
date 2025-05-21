f41c099c39ee60ea3bfb1bdb08429932fd714fcd
shorres <stratton.horres@prizepicks.com>
2025-05-20 23:38:53 -0500

    Modify Soldier Shape

--- src/shapes/SoldierShape.js
+++ src/shapes/SoldierShape.js
@@ -1,12 +1,7 @@
 import PropTypes from 'prop-types';
 
 import CharacterShape from "./CharacterShape";
-import { abilities } from '../data/CharacterFeatures';
-import { ABILITIES } from "../data/Misc";
 
 export default PropTypes.shape({
-  ...CharacterShape,
-  isSpecialist: PropTypes.bool.isRequired,
-  cost: PropTypes.number.isRequired,
-  [ABILITIES]: PropTypes.arrayOf(PropTypes.oneOf(abilities))
+  ...CharacterShape
 })
