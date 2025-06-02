46d24d0f86f9ef0151d4f9118b2be201d059aefd
shorres <stratton.horres@prizepicks.com>
2025-06-02 13:56:41 -0500

    Groundwork and initial commit for React95 version

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import 'react-tooltip/dist/react-tooltip.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+//const root = createRoot(container);
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+<App />, container
 );
 
 
